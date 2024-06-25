1c5cbd8bab40e98788c839eadb233296279877b0
a.vladimirov <a.vladimirov@unlimint.com>
2024-06-25 13:36:45 +0300

    no message

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/traefik/plugindemo
+module github.com/bublicov/traefik-x-request-id
 
 go 1.19
+
+require github.com/google/uuid v1.6.0 // indirect
---
+++ x_request_id.go
@@ -0,0 +1,43 @@
+package traefik_x_request_id
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/google/uuid"
+)
+
+// Config the plugin configuration.
+type Config struct {
+}
+
+// CreateConfig creates the default plugin configuration.
+func CreateConfig() *Config {
+	return &Config{}
+}
+
+// RequestID a RequestID plugin.
+type RequestID struct {
+	next http.Handler
+	name string
+}
+
+// New created a new RequestID plugin.
+func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
+	return &RequestID{
+		next: next,
+		name: name,
+	}, nil
+}
+
+// ServeHTTP implements the http.Handler interface.
+func (a *RequestID) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	// Generate a unique request ID
+	requestID := uuid.New().String()
+
+	// Set the request ID in the request header
+	req.Header.Set("X-Request-ID", requestID)
+
+	// Call the next handler
+	a.next.ServeHTTP(rw, req)
+}
